7fbb6222deed9b353451408056d5316ddc722ee6
Simmi <simmikukreya123@gmail.com>
2023-12-08 10:04:43 +0700

    q-input 3 inputs

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,16 +6,24 @@
       @reset="onReset"
       class="q-gutter-md"
     >
-      <q-input
+    <q-input
         filled
         v-model="name"
         label="Your name *"
         hint="Name and surname"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
+    />
+    <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+    />
 
-      <q-input
+    <q-input
         filled
         type="number"
         v-model="age"
@@ -25,7 +33,7 @@
           val => val !== null && val !== '' || 'Please type your age',
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
-      />
+    />
 
       <q-toggle v-model="accept" label="I accept the license and terms" />
 
